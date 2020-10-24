463b12d5100040be40fcf0b06c0b5aa9a58d9e5e
pshtony1 <pshtony1@naver.com>
2020-10-24 12:30:21 +0900

    is last?

--- src/middlewares.js
+++ src/middlewares.js
@@ -6,6 +6,7 @@ import routes from "./routes";
 const s3 = new aws.S3({
   accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  region: "ap-northeast-1",
 });
 
 const multerVideo = multer({
