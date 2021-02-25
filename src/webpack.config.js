d4ef42b96ad440453677637f5ece0c200948f2cd
Feca <pshtony1@naver.com>
2021-02-25 19:16:18 +0900

    omg

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,13 +1,14 @@
 const path = require("path");
+
+// const ExtractCSS = require("extract-text-webpack-plugin");
+const MiniExtractCSS = require("mini-css-extract-plugin");
 const autoprefixer = require("autoprefixer");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  devtool: "cheap-module-source-map",
   entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
@@ -21,32 +22,29 @@ const config = {
         ],
       },
       {
-        test: /\.(scss)$/,
+        test: /\.scss$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
+            loader: MiniExtractCSS.loader,
+            options: {
+              hmr: process.env.WEBPACK_ENV === "development",
+            },
           },
-          {
-            loader: "css-loader",
-          },
+          "css-loader",
           {
             loader: "postcss-loader",
             options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    "autoprefixer",
-                    {
-                      overrideBrowserslist: "cover 99.5%",
-                    },
-                  ],
-                ],
+              plugins() {
+                return [
+                  autoprefixer({
+                    overrideBrowserslist: "cover 99.5%",
+                  }),
+                ];
               },
             },
           },
-          {
-            loader: "sass-loader",
-          },
+
+          "sass-loader",
         ],
       },
     ],
@@ -56,9 +54,7 @@ const config = {
     filename: "[name].js",
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
+    new MiniExtractCSS({
       filename: "styles.css",
     }),
   ],
